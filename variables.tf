9ada4352871025f575351ecc907b7147bf0fb08e
ptonini <pedro.tonini@hotmail.com>
2023-12-11 18:00:47 -0300

    fix: minor refactor

--- variables.tf
+++ variables.tf
@@ -1,7 +1,25 @@
 variable "name" {}
 
-variable "rg" {}
+variable "rg" {
+  type = object({
+    name = string
+    location = string
+  })
+}
+
+variable "priority_offset" {
+  default = 100
+}
 
 variable "network_rules" {
+  type = map(object({
+    protocol                    = string
+    direction                   = optional(string, "Inbound")
+    access                      = optional(string, "Allow")
+    source_port_range           = optional(string, "*")
+    destination_port_range      = optional(string, "*")
+    source_address_prefix       = optional(string, "*")
+    destination_address_prefix  = optional(string, "*")
+  }))
   default = {}
 }
